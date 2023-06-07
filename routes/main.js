a4a465d4c17f198cd257c3760ba83c4581b49eea
Gilbert <wolfwizard52@gmail.com>
2023-06-07 21:26:37 +0300

    Latest Update

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const axios = require("axios")
 
+//home route
+router.get("/", (req,res)=>{
+      res.json({msg:"Welcome Home Isingiro Server"})
+})
 //view_settings
 router.get("/settings", async (req,res)=>{
       const response = await axios.get("https://muzboxafrica.com/api2/isingiro_fm/settings/")
